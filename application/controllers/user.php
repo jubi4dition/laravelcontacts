f0b2145574d3e09910b59a14f947a9a0a4be4265
Jubi Dition <yubi4dition@googlemail.com>
2012-07-21 13:13:22 +0200

    indent

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -4,7 +4,7 @@ class User_Controller extends Base_Controller {
 
 	public function __construct()
 	{
-    	parent::__construct();
+		parent::__construct();
 		$this->filter('before', 'logged_in');
 	}
 
