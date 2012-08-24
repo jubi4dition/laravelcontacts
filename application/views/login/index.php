203506aed26e10ac7ff1bf9d4d7015a75896aa44
Jubi Dition <yubi4dition@googlemail.com>
2012-08-24 18:21:25 +0200

    update view to 3.2.5 and 2.1.0

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -48,7 +48,7 @@
 	});
 	</script>
 	<footer>
-		<p>&copy; 2012 built with Laravel 3.2.3 and Twitter Bootstrap 2.0.4</p>
+		<p>&copy; 2012 built with Laravel 3.2.5 and Twitter Bootstrap 2.1.0</p>
 	</footer>
 </div><!-- /container -->
 </body>
