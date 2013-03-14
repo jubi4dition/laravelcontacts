d5775b0c90bbe59122b92c4fe769e464953291ad
Jubi Dition <yubi4dition@googlemail.com>
2013-03-14 23:57:32 +0100

    tabs to spaces

--- application/controllers/user/contacts.php
+++ application/controllers/user/contacts.php
@@ -2,56 +2,56 @@
 
 class User_Contacts_Controller extends Base_Controller {
 
-	public $restful = true;
+    public $restful = true;
 
-	public function get_index()
-	{
-		$contacts = Contact::where('uid', '=', Session::get('uid'))->get();
+    public function get_index()
+    {
+        $contacts = Contact::where('uid', '=', Session::get('uid'))->get();
 
-		return View::make('contacts.index')->with('contacts', $contacts); 
-	}
+        return View::make('contacts.index')->with('contacts', $contacts); 
+    }
 
-	public function get_add()
-	{
-		return View::make('contacts.add');
-	}
+    public function get_add()
+    {
+        return View::make('contacts.add');
+    }
 
-	public function post_add()
-	{
-		sleep(1);
-		
-		$rules = array(
-			'name' => 'required|max:60|alpha_dash',
-			'email' => 'required|max:60|email',
-			'phone' => 'required|max:30|alpha_num'
-		);
+    public function post_add()
+    {
+        sleep(1);
+        
+        $rules = array(
+            'name' => 'required|max:60|alpha_dash',
+            'email' => 'required|max:60|email',
+            'phone' => 'required|max:30|alpha_num'
+        );
 
-		$validation = Validator::make(Input::get(), $rules);
-		
-		if ($validation->fails()) {
-			$message = "<strong>Adding</strong> failed!";
+        $validation = Validator::make(Input::get(), $rules);
+        
+        if ($validation->fails()) {
+            $message = "<strong>Adding</strong> failed!";
 
-			return Helper::json(false, $message);
-		} else {
-			$name = Input::get('name');
+            return Helper::json(false, $message);
+        } else {
+            $name = Input::get('name');
 
-			$created = Contact::create(array(
-				'uid' => Session::get('uid'),
-				'name' => $name,
-				'email' => Input::get('email'),
-				'phone' => Input::get('phone')
-			));
+            $created = Contact::create(array(
+                'uid' => Session::get('uid'),
+                'name' => $name,
+                'email' => Input::get('email'),
+                'phone' => Input::get('phone')
+            ));
 
-			if ($created) {
-				$message = "<strong>".$name."</strong> has been added!";
-				
-				return Helper::json(true, $message);
-			} else {
-				$message = "<strong>".$name."</strong> already exists!";
-				
-				return Helper::json(false, $message);
-			}
-			
-		}	
-	}
+            if ($created) {
+                $message = "<strong>".$name."</strong> has been added!";
+                
+                return Helper::json(true, $message);
+            } else {
+                $message = "<strong>".$name."</strong> already exists!";
+                
+                return Helper::json(false, $message);
+            }
+            
+        }   
+    }
 }
--- application/views/includes/navbar.php
+++ application/views/includes/navbar.php
@@ -1,22 +1,22 @@
 <div class="navbar navbar-static-top">
-	<div class="navbar-inner">
-		<div class="container">
-			<a class="brand" href="<?=URL::to('user'); ?>"><img src="<?=URL::to_asset('img/yourcontacts.png'); ?>" width="57px"/></a>
-			<ul class="nav">
-				<li class="divider-vertical"></li>
-				<li><?=HTML::link('user/contacts', 'Contacts'); ?></li>
-				<li class="divider-vertical"></li>
-				<li><?=HTML::link('user/contacts/add', 'Add'); ?></li>
-				<li class="divider-vertical"></li>
-				<li><?=HTML::link('user/delete', 'Delete'); ?></li>
-				<li class="divider-vertical"></li>
-				<li><?=HTML::link('user/edit', 'Edit'); ?></li>
-				<li class="divider-vertical"></li>
-			</ul>
-			<div class="pull-right">
-				<small class="navbar-text">User: <?=HTML::link('user/profile', Session::get('email')); ?> </small>
-				<a href="<?=URL::to('login/logout') ?>" class="btn btn-primary"><i class="icon-road icon-white"></i> Logout</a>
-			</div>
-		</div>
-	</div>
+  <div class="navbar-inner">
+    <div class="container">
+      <a class="brand" href="<?=URL::to('user'); ?>"><img src="<?=URL::to_asset('img/yourcontacts.png'); ?>" width="57px"/></a>
+      <ul class="nav">
+        <li class="divider-vertical"></li>
+        <li><?=HTML::link('user/contacts', 'Contacts'); ?></li>
+        <li class="divider-vertical"></li>
+        <li><?=HTML::link('user/contacts/add', 'Add'); ?></li>
+        <li class="divider-vertical"></li>
+        <li><?=HTML::link('user/delete', 'Delete'); ?></li>
+        <li class="divider-vertical"></li>
+        <li><?=HTML::link('user/edit', 'Edit'); ?></li>
+        <li class="divider-vertical"></li>
+      </ul>
+      <div class="pull-right">
+        <small class="navbar-text">User: <?=HTML::link('user/profile', Session::get('email')); ?> </small>
+        <a href="<?=URL::to('login/logout') ?>" class="btn btn-primary"><i class="icon-road icon-white"></i> Logout</a>
+      </div>
+    </div>
+  </div>
 </div>
