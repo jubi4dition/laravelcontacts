c47bce8e770ec6b37ed79991208ac5bed6a0fec1
Jubi Dition <yubi4dition@googlemail.com>
2013-05-20 21:54:22 +0200

    update message login

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -6,7 +6,7 @@
     <h1>Login</h1>
   </div>
   <div class="row">
-    <div class="span5 offset3">
+    <div class="span6 offset3">
       <form class="well" action="<?=Url::to('login/check'); ?>" method="post" accept-charset="utf-8">
         <input type="text" class="input-block-level" name="email" value="Email" placeholder="Email" required maxlength="60" autofocus>
         <input type="password" class="input-block-level" name="pwd" placeholder="Password" required maxlength="30">
@@ -17,15 +17,15 @@
   </div>
   <? if (isset($error)): ?>
   <div class="row">
-    <div class="span5 offset3">
+    <div class="span6 offset3">
       <div class="alert alert-error">
-        <strong>Login</strong> failed!.
+        <b>Login</b> failed! The <b>Email</b> or <b>Password</b> is not correct!
       </div>
     </div>
   </div>
   <? else: ?>
   <div class="row">
-    <div class="span5 offset3">
+    <div class="span6 offset3">
       <div class="alert alert-info">
         <p><strong>You</strong> are not logged in!</p>
         <small>No Account? </small>
