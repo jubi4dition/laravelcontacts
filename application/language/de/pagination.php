5ce067811a80b0a7469342ecba5c62101cb824b0
Jubi Dition <yubi4dition@googlemail.com>
2013-05-27 21:40:43 +0200

    update languages

--- application/language/de/pagination.php
+++ application/language/de/pagination.php
@@ -13,7 +13,7 @@ return array(
     |
     */
 
-    'previous' => '&laquo; Previous',
-    'next'     => 'Next &raquo;',
+    'previous' => '&laquo; Zur&uuml;ck',
+    'next'     => 'Weiter &raquo;',
 
 );
