a7a6f7473e1ccb735d0935473ba3ab047ce17263
Jubi Dition <yubi4dition@googlemail.com>
2013-05-29 22:43:27 +0200

    jquery-2.0.1

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,3 +1,3 @@
 <footer>
-  <p>&copy; 2013 built with Laravel v3.2.13, Bootstrap v2.3.1 and jQuery v1.9.1</p>
+  <p>&copy; 2013 built with Laravel v3.2.13, Bootstrap v2.3.1 and jQuery v2.0.1</p>
 </footer>
